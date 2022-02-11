f02d3fc36f10c7293f16357016d54014c805154c
Ian Braun <ibraun@c-path.org>
2022-02-11 09:49:27 -0600

    adding reasoning step mapping script

--- data_mapping/scripts/create_and_load_triples.py
+++ data_mapping/scripts/create_and_load_triples.py
@@ -9,49 +9,43 @@ import os
 working_dir = os.getcwd()
 
 # Input paths and files.
-#mappings_dir = "mappings"
-#yarrrml_template_filename = "mapping_template.yml"
 yarrrml_template_path = os.path.join("data_mapping", "mappings", "mapping_template.yml")
 ontology_ttl_path = os.path.join("data_mapping", "turtle", "biolink_sdtm_owl.ttl")
 
 
 # Output paths and files.
 output_dir = "output"
-#rml_template_filename = "rml_template.ttl"
-#instance_data_filename = "triples.ttl"
 rml_template_output_path = os.path.join("data_mapping", "turtle", "rml_template.ttl")
 instance_data_output_path = os.path.join("data_mapping", "turtle", "instance_data_triples.ttl")
 output_ttl_path = os.path.join("data_mapping", "turtle", "biolink_sdtm_owl_with_data.ttl")
-
-
+output_ttl_path_reasoned = os.path.join("data_mapping", "turtle", "biolink_sdtm_owl_with_data_reasoned.ttl")
 
 
 convert_paths_for_docker = lambda path: path.replace("\\","/")
 
 
-
-
-
-# Convert the YARRRML mapping template to a RML mapping template.
+# 1. Convert the YARRRML mapping template to a RML mapping template.
 subprocess.run(["docker", "run", "--rm", "-it", 
                 "-v", f"{working_dir}:/data", 
                 "rmlio/yarrrml-parser:latest", 
                 "-i", f"/data/{convert_paths_for_docker(yarrrml_template_path)}", 
-                "-o", f"/data/{convert_paths_for_docker(rml_template_output_path)}"])
+                "-o", f"/data/{convert_paths_for_docker(rml_template_output_path)}"
+                ])
 
 
 
-# Use the RML mapping template to process the csv's and output the data triples as a ttl file.
+# 2. Use the RML mapping template to process the csv's and output the data triples as a ttl file.
 subprocess.run(["docker", "run", "--rm", 
                 "-v", f"{working_dir}:/data", 
                 "rmlio/rmlmapper-java", 
                 "-m", f"{convert_paths_for_docker(rml_template_output_path)}",  
-                "-o", f"{convert_paths_for_docker(instance_data_output_path)}"])
+                "-o", f"{convert_paths_for_docker(instance_data_output_path)}"
+                ])
 
 
 
 
-# Create a combined ttl file for the ontology and the instance data.
+# 3. Create a combined ttl file for the ontology and the instance data.
 with open(instance_data_output_path, "r") as f:
     data_triples = f.read()
 
@@ -62,3 +56,17 @@ with open(output_ttl_path, "w") as f:
     f.write(f"{ontology_content}\n\n\n{data_triples}")
 
 
+
+
+# 4. Reason over the combined ontology and data triples and save the output ttl file.
+subprocess.run(["docker", "run", 
+                "-v", f"{working_dir}:/work",
+                "--rm", "-ti",
+                "obolibrary/robot", "robot", "reason",
+                "--reasoner", "hermit",
+                "--axiom-generators", '"SubClass EquivalentClass ClassAssertion PropertyAssertion"',
+                "--input", f"/work/{convert_paths_for_docker(output_ttl_path)}",
+                "--include-indirect", "true",
+                "reduce",
+                "--output", f"/work/{convert_paths_for_docker(output_ttl_path_reasoned)}"
+                ])
